8d2d166cb20e783ee13cf621fb3560994b001d49
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2016-07-18 11:02:03 +0300

    Tests added.

---
+++ src/test/SquareMatrixTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SquareMatrixTest {
+
+	@Test
+	public void testSquareMatrixIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSquareMatrixDoubleArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumOfRows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumOfColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
